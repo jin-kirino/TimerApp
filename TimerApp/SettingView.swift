0aef7d2c95885445b8cc262045428698dd96f228
jin-kirino <jinkirino@gmail.com>
2023-04-16 22:13:37 +0900

    Pickerを追加

--- TimerApp/SettingView.swift
+++ TimerApp/SettingView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State private var timerValue: Int = 10
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Picker(selection: $timerValue) {
+                Text("10")
+                    .tag(10)
+                Text("20")
+                    .tag(20)
+                Text("30")
+                    .tag(30)
+                Text("40")
+                    .tag(40)
+                Text("50")
+                    .tag(50)
+                Text("60")
+                    .tag(60)
+            } label: {
+                Text("選択")
+            }
+            .pickerStyle(.wheel)
+        }
     }
 }
 
