7705ae1a909d85ccda2bb518d018bd69ba064d57
jin-kirino <jinkirino@gmail.com>
2023-04-16 21:50:04 +0900

    SettingViewを追加

---
+++ TimerApp/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  TimerApp
+//
+//  Created by 神　樹里乃 on 2023/04/16.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
