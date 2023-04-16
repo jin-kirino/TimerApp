acd34e33010a1fd596b73e93c45dab23bcf9c2a9
jin-kirino <jinkirino@gmail.com>
2023-04-16 21:48:13 +0900

    first push

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by 神　樹里乃 on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
